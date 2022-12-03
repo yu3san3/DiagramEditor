157ad5fb1de05f992983488f43a04dbf3d5fed81
NIWA-Yuichiro <20r5068@moon.aichi-u.ac.jp>
2022-12-04 03:25:19 +0900

    コメントを修正

--- DiagramEditor/DiagramEditorDocument.swift
+++ DiagramEditor/DiagramEditorDocument.swift
@@ -21,19 +21,19 @@ struct DiagramEditorDocument: FileDocument {
         self.text = text
     }
 
-    static var readableContentTypes: [UTType] { [.oudiaEditDocument] } //ドキュメントが開くことができるタイプを設定
+    static var readableContentTypes: [UTType] { [.oudiaEditDocument] } //開くことができるドキュメントのタイプを設定
 
     init(configuration: ReadConfiguration) throws { //ファイルの読み込みを担当
         guard let data = configuration.file.regularFileContents,
-              let string = String(data: data, encoding: .shiftJIS) //utf8だったが、それだと本家oudiaで作ったファイルが読み込めない
+              let string = String(data: data, encoding: .shiftJIS) //Shift-JIS Stringに変換
         else {
-            throw CocoaError(.fileReadCorruptFile)
+            throw CocoaError(.fileReadCorruptFile) //utf-8など、Shift-JIS以外の形式で保存されているファイルは開けない
         }
         text = string
     }
     
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper { //ファイルの保存を担当
-        let data = text.data(using: .shiftJIS)! //utf8だったが、それだと本家oudiaで作ったファイルが読み込めない
-        return .init(regularFileWithContents: data)
+        let data = text.data(using: .shiftJIS)!
+        return .init(regularFileWithContents: data) //Shift-JIS Dataで保存
     }
 }
