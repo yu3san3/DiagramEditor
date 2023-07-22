de6b3a04e8ce6e962c3dc317d433616dd39f49f9
yu3san3 <orange12817@yahoo.co.jp>
2023-07-22 16:36:38 +0900

    stringifyを細かなメソッドに分けた

--- DiagramEditor/Other/OudParser.swift
+++ DiagramEditor/Other/OudParser.swift
@@ -276,132 +276,137 @@ class OuDia {
     static func stringify(_ data: OudData) -> String {
         var result: String = ""
         result.append("FileType=\(data.fileType)\n") //OudDataの情報を順番に追加していく
-        result.append("Rosen.\n")
-        result.append("Rosenmei=\(data.rosen.rosenmei)\n")
-        for i in 0..<data.rosen.eki.count {
-            result.append("Eki.\n")
-            result.append("Ekimei=\(data.rosen.eki[i].ekimei)\n")
-            result.append("Ekijikokukeisiki=\(data.rosen.eki[i].ekijikokukeisiki.rawValue)\n")
-            result.append("Ekikibo=\(data.rosen.eki[i].ekikibo.rawValue)\n")
-            if !data.rosen.eki[i].kyoukaisen.isEmpty {
-                result.append("Kyoukaisen=\(data.rosen.eki[i].kyoukaisen)\n")
-            }
-            if !data.rosen.eki[i].diagramRessyajouhouHyoujiKudari.isEmpty {
-                result.append("DiagramRessyajouhouHyoujiKudari=\(data.rosen.eki[i].diagramRessyajouhouHyoujiKudari)\n")
-            }
-            if !data.rosen.eki[i].diagramRessyajouhouHyoujiNobori.isEmpty {
-                result.append("DiagramRessyajouhouHyoujiNobori=\(data.rosen.eki[i].diagramRessyajouhouHyoujiNobori)\n")
-            }
-            result.append(".\n") //Eki. End
-        }
-        for i in 0..<data.rosen.ressyasyubetsu.count {
-            result.append("Ressyasyubetsu.\n")
-            result.append("Syubetsumei=\(data.rosen.ressyasyubetsu[i].syubetsumei)\n")
-            result.append("Ryakusyou=\(data.rosen.ressyasyubetsu[i].ryakusyou)\n")
-            result.append("JikokuhyouMojiColor=\(data.rosen.ressyasyubetsu[i].jikokuhyouMojiColor)\n")
-            result.append("JikokuhyouFontIndex=\(data.rosen.ressyasyubetsu[i].jikokuhyouFontIndex)\n")
-            result.append("DiagramSenColor=\(data.rosen.ressyasyubetsu[i].diagramSenColor)\n")
-            result.append("DiagramSenStyle=\(data.rosen.ressyasyubetsu[i].diagramSenStyle.rawValue)\n")
-            if !data.rosen.ressyasyubetsu[i].diagramSenIsBold.isEmpty {
-                result.append("DiagramSenIsBold=\(data.rosen.ressyasyubetsu[i].diagramSenIsBold)\n")
-            }
-            if !data.rosen.ressyasyubetsu[i].stopMarkDrawType.isEmpty {
-                result.append("StopMarkDrawType=\(data.rosen.ressyasyubetsu[i].stopMarkDrawType)\n")
-            }
-            result.append(".\n") //Ressyasyubetsu. End
-        }
-        for i in 0..<data.rosen.dia.count {
-            result.append("Dia.\n")
-            result.append("DiaName=\(data.rosen.dia[i].diaName)\n")
-            result.append("Kudari.\n")
-            for j in 0..<data.rosen.dia[i].kudari.ressya.count {
-                result.append("Ressya.\n")
-                if !data.rosen.dia[i].kudari.ressya[j].houkou.isEmpty {
-                    result.append("Houkou=\(data.rosen.dia[i].kudari.ressya[j].houkou)\n")
-                    result.append("Syubetsu=\(data.rosen.dia[i].kudari.ressya[j].syubetsu)\n")
-                }
-                if !data.rosen.dia[i].kudari.ressya[j].ressyabangou.isEmpty {
-                    result.append("Ressyabangou=\(data.rosen.dia[i].kudari.ressya[j].ressyabangou)\n")
-                }
-                if !data.rosen.dia[i].kudari.ressya[j].ressyamei.isEmpty {
-                    result.append("Ressyamei=\(data.rosen.dia[i].kudari.ressya[j].ressyamei)\n")
-                }
-                if !data.rosen.dia[i].kudari.ressya[j].gousuu.isEmpty {
-                    result.append("Gousuu=\(data.rosen.dia[i].kudari.ressya[j].gousuu)\n")
-                }
-                if !data.rosen.dia[i].kudari.ressya[j].ekiJikoku.isEmpty {
-                    result.append("EkiJikoku=\(EkiJikoku.stringify(data.rosen.dia[i].kudari.ressya[j].ekiJikoku))\n")
-                }
-                if !data.rosen.dia[i].kudari.ressya[j].bikou.isEmpty {
-                    result.append("Bikou=\(data.rosen.dia[i].kudari.ressya[j].bikou)\n")
-                }
-                result.append(".\n") //Ressya. End
-            }
-            result.append(".\n") //Kudari. End
-            result.append("Nobori.\n")
-            for j in 0..<data.rosen.dia[i].nobori.ressya.count {
-                result.append("Ressya.\n")
-                if !data.rosen.dia[i].nobori.ressya[j].houkou.isEmpty {
-                    result.append("Houkou=\(data.rosen.dia[i].nobori.ressya[j].houkou)\n")
-                    result.append("Syubetsu=\(data.rosen.dia[i].nobori.ressya[j].syubetsu)\n")
-                }
-                if !data.rosen.dia[i].nobori.ressya[j].ressyabangou.isEmpty {
-                    result.append("Ressyabangou=\(data.rosen.dia[i].nobori.ressya[j].ressyabangou)\n")
-                }
-                if !data.rosen.dia[i].nobori.ressya[j].ressyamei.isEmpty {
-                    result.append("Ressyamei=\(data.rosen.dia[i].nobori.ressya[j].ressyamei)\n")
-                }
-                if !data.rosen.dia[i].nobori.ressya[j].gousuu.isEmpty {
-                    result.append("Gousuu=\(data.rosen.dia[i].nobori.ressya[j].gousuu)\n")
-                }
-                if !data.rosen.dia[i].nobori.ressya[j].ekiJikoku.isEmpty {
-                    result.append("EkiJikoku=\(EkiJikoku.stringify(data.rosen.dia[i].nobori.ressya[j].ekiJikoku))\n") //[String] -> Stringに変換して代入
-                }
-                if !data.rosen.dia[i].nobori.ressya[j].bikou.isEmpty {
-                    result.append("Bikou=\(data.rosen.dia[i].nobori.ressya[j].bikou)\n")
-                }
-                result.append(".\n") //Ressya. End
-            }
-            result.append(".\n") //Nobori. End
-            result.append(".\n") //Dia. End
-        }
-        result.append("KitenJikoku=\(data.rosen.kitenJikoku)\n")
-        result.append("DiagramDgrYZahyouKyoriDefault=\(data.rosen.diagramDgrYZahyouKyoriDefault)\n")
-        result.append("Comment=\(data.rosen.comment)\n")
-        result.append(".\n") //Rosen End
-        result.append("DispProp.\n")
-        for i in 0..<data.dispProp.jikokuhyouFont.count {
-            result.append("JikokuhyouFont=\(data.dispProp.jikokuhyouFont[i])\n")
-        }
-        result.append("JikokuhyouVFont=\(data.dispProp.jikokuhyouVFont)\n")
-        result.append("DiaEkimeiFont=\(data.dispProp.diaEkimeiFont)\n")
-        result.append("DiaJikokuFont=\(data.dispProp.diaJikokuFont)\n")
-        result.append("DiaRessyaFont=\(data.dispProp.diaRessyaFont)\n")
-        result.append("CommentFont=\(data.dispProp.commentFont)\n")
-        result.append("DiaMojiColor=\(data.dispProp.diaMojiColor)\n")
-        result.append("DiaHaikeiColor=\(data.dispProp.diaHaikeiColor)\n")
-        result.append("DiaRessyaColor=\(data.dispProp.diaRessyaColor)\n")
-        result.append("DiaJikuColor=\(data.dispProp.diaJikuColor)\n")
-        result.append("EkimeiLength=\(data.dispProp.ekimeiLength)\n")
-        result.append("JikokuhyouRessyaWidth=\(data.dispProp.jikokuhyouRessyaWidth)\n")
-        result.append(".\n") //DispProp End
+        stringifyRosen(rosen: data.rosen)
+        stringifyDispProp(dispProp: data.dispProp)
         result.append("FileTypeAppComment=" + "Diagram Editor Ver. Alpha 1.0.0") //ここは各Appが名付ける要素
-        
         return result
+
+        func stringifyRosen(rosen: Rosen) {
+            result.append("Rosen.\n")
+            result.append("Rosenmei=\(rosen.rosenmei)\n")
+            stringifyEki(ekiArr: rosen.eki)
+            stringifyRessyasyubetsu(ressyasyubetsuArr: rosen.ressyasyubetsu)
+            stringifyDia(diaArr: rosen.dia)
+            result.append("KitenJikoku=\(rosen.kitenJikoku)\n")
+            result.append("DiagramDgrYZahyouKyoriDefault=\(rosen.diagramDgrYZahyouKyoriDefault)\n")
+            result.append("Comment=\(rosen.comment)\n")
+            result.append(".\n") //Rosen End
+            return
+
+            func stringifyEki(ekiArr: [Eki]) {
+                for eki in ekiArr {
+                    result.append("Eki.\n")
+                    result.append("Ekimei=\(eki.ekimei)\n")
+                    result.append("Ekijikokukeisiki=\(eki.ekijikokukeisiki.rawValue)\n")
+                    result.append("Ekikibo=\(eki.ekikibo.rawValue)\n")
+                    if !eki.kyoukaisen.isEmpty {
+                        result.append("Kyoukaisen=\(eki.kyoukaisen)\n")
+                    }
+                    if !eki.diagramRessyajouhouHyoujiKudari.isEmpty {
+                        result.append("DiagramRessyajouhouHyoujiKudari=\(eki.diagramRessyajouhouHyoujiKudari)\n")
+                    }
+                    if !eki.diagramRessyajouhouHyoujiNobori.isEmpty {
+                        result.append("DiagramRessyajouhouHyoujiNobori=\(eki.diagramRessyajouhouHyoujiNobori)\n")
+                    }
+                    result.append(".\n") //Eki. End
+                }
+                return
+            }
+
+            func stringifyRessyasyubetsu(ressyasyubetsuArr: [Ressyasyubetsu]) {
+                for ressyasyubetsu in ressyasyubetsuArr {
+                    result.append("Ressyasyubetsu.\n")
+                    result.append("Syubetsumei=\(ressyasyubetsu.syubetsumei)\n")
+                    result.append("Ryakusyou=\(ressyasyubetsu.ryakusyou)\n")
+                    result.append("JikokuhyouMojiColor=\(ressyasyubetsu.jikokuhyouMojiColor)\n")
+                    result.append("JikokuhyouFontIndex=\(ressyasyubetsu.jikokuhyouFontIndex)\n")
+                    result.append("DiagramSenColor=\(ressyasyubetsu.diagramSenColor)\n")
+                    result.append("DiagramSenStyle=\(ressyasyubetsu.diagramSenStyle.rawValue)\n")
+                    if !ressyasyubetsu.diagramSenIsBold.isEmpty {
+                        result.append("DiagramSenIsBold=\(ressyasyubetsu.diagramSenIsBold)\n")
+                    }
+                    if !ressyasyubetsu.stopMarkDrawType.isEmpty {
+                        result.append("StopMarkDrawType=\(ressyasyubetsu.stopMarkDrawType)\n")
+                    }
+                    result.append(".\n") //Ressyasyubetsu. End
+                }
+                return
+            }
+
+            func stringifyDia(diaArr: [Dia]) {
+                for dia in diaArr {
+                    result.append("Dia.\n")
+                    result.append("DiaName=\(dia.diaName)\n")
+                    result.append("Kudari.\n")
+                    stringifyRessya(ressyaArr: dia.kudari.ressya)
+                    result.append(".\n") //Kudari. End
+                    result.append("Nobori.\n")
+                    stringifyRessya(ressyaArr: dia.nobori.ressya)
+                    result.append(".\n") //Nobori. End
+                    result.append(".\n") //Dia. End
+                }
+                return
+
+                func stringifyRessya(ressyaArr: [Ressya]) {
+                    for ressya in ressyaArr {
+                        result.append("Ressya.\n")
+                        if !ressya.houkou.isEmpty {
+                            result.append("Houkou=\(ressya.houkou)\n")
+                            result.append("Syubetsu=\(ressya.syubetsu)\n")
+                        }
+                        if !ressya.ressyabangou.isEmpty {
+                            result.append("Ressyabangou=\(ressya.ressyabangou)\n")
+                        }
+                        if !ressya.ressyamei.isEmpty {
+                            result.append("Ressyamei=\(ressya.ressyamei)\n")
+                        }
+                        if !ressya.gousuu.isEmpty {
+                            result.append("Gousuu=\(ressya.gousuu)\n")
+                        }
+                        if !ressya.ekiJikoku.isEmpty {
+                            result.append("EkiJikoku=\( EkiJikoku.stringify(ressya.ekiJikoku) )\n")
+                        }
+                        if !ressya.bikou.isEmpty {
+                            result.append("Bikou=\(ressya.bikou)\n")
+                        }
+                        result.append(".\n") //Ressya. End
+                    }
+                    return
+                }
+            }
+        }
+
+        func stringifyDispProp(dispProp: DispProp) {
+            result.append("DispProp.\n")
+            for jikokuhyouFont in dispProp.jikokuhyouFont {
+                result.append("JikokuhyouFont=\(jikokuhyouFont)\n")
+            }
+            result.append("JikokuhyouVFont=\(dispProp.jikokuhyouVFont)\n")
+            result.append("DiaEkimeiFont=\(dispProp.diaEkimeiFont)\n")
+            result.append("DiaJikokuFont=\(dispProp.diaJikokuFont)\n")
+            result.append("DiaRessyaFont=\(dispProp.diaRessyaFont)\n")
+            result.append("CommentFont=\(dispProp.commentFont)\n")
+            result.append("DiaMojiColor=\(dispProp.diaMojiColor)\n")
+            result.append("DiaHaikeiColor=\(dispProp.diaHaikeiColor)\n")
+            result.append("DiaRessyaColor=\(dispProp.diaRessyaColor)\n")
+            result.append("DiaJikuColor=\(dispProp.diaJikuColor)\n")
+            result.append("EkimeiLength=\(dispProp.ekimeiLength)\n")
+            result.append("JikokuhyouRessyaWidth=\(dispProp.jikokuhyouRessyaWidth)\n")
+            result.append(".\n") //DispProp End
+            return
+        }
     }
 }
 
 private class EkiJikoku {
     static func parse(_ text: String) -> [String] {
-        var result: [String] = []
-        result = text.components(separatedBy: ",")
-        return result
+        return text.components(separatedBy: ",")
     }
     
-    static func stringify(_ array: [String]) -> String {
+    static func stringify(_ jikokuArr: [String]) -> String {
         var result: String = ""
-        for i in 0 ..< array.count {
-            result += array[i] + ","
+        for jikoku in jikokuArr {
+            result += jikoku + ","
         }
         if !result.isEmpty {
             result.removeLast()
