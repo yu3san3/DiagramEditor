e32ed0c9e085e6a14fe8789dccb103e494709a62
yu3san3 <orange12817@yahoo.co.jp>
2023-12-03 22:05:50 +0900

    SidebarViewの要素を選択しても、いちいちDisclosureGroupが閉じないようにした。

--- DiagramEditor/View/SidebarView.swift
+++ DiagramEditor/View/SidebarView.swift
@@ -14,60 +14,46 @@ struct SidebarView: View {
 
     var body: some View {
         List {
-            ForEach(makeSidebarContents()) { content in
-                sidebarView(content)
+            DisclosureGroup("路線") {
+                Text("駅")
+                    .onTapGesture {
+                        detailViewStatus = .eki
+                    }
+                Text("列車種別")
+                    .onTapGesture {
+                        detailViewStatus = .ressyasyubetsu
+                    }
+                DisclosureGroup("ダイヤ") {
+                    ForEach(Array(document.oudData.rosen.dia.enumerated()),
+                            id: \.1.id
+                    ) { index, dia in
+                        DisclosureGroup(dia.diaName) {
+                            makeDiaListElement(diaNum: index)
+                        }
+                    }
+                }
             }
         }
         .listStyle(.sidebar)
     }
-
-    func sidebarView(_ content: SidebarContent) -> some View {
-        if let children = content.children {
-            return AnyView(DisclosureGroup(content.name) {
-                ForEach(children) { child in
-                    sidebarView(child)
-                }
-            })
-        } else {
-            return AnyView(
-                HStack {
-                    Image(systemName: content.icon)
-                    Text(content.name)
-                }
-                    .onTapGesture {
-                        detailViewStatus = content.detailViewStatus ?? .none
-                    }
-            )
-        }
-    }
 }
 
 private extension SidebarView {
-    func makeSidebarContents() -> [SidebarContent] {
-        var dias: [SidebarContent] = []
-        for (index, dia) in document.oudData.rosen.dia.enumerated() {
-            dias.append(
-                SidebarContent(name: dia.diaName, icon: "doc", detailViewStatus: nil, children: [
-                    SidebarContent(name: "下り時刻表", icon: "doc", detailViewStatus: .jikokuhyou(houkou: .kudari, diaNum: index), children: nil),
-                    SidebarContent(name: "上り時刻表", icon: "doc", detailViewStatus: .jikokuhyou(houkou: .nobori, diaNum: index), children: nil),
-                    SidebarContent(name: "ダイヤグラム", icon: "doc", detailViewStatus: .diagram, children: nil),
-                ])
-            )
-        }
-        return [SidebarContent(name: "路線", icon: "folder", detailViewStatus: nil, children: [
-            SidebarContent(name: "駅", icon: "doc", detailViewStatus: .eki, children: nil),
-            SidebarContent(name: "列車種別", icon: "doc", detailViewStatus: .ressyasyubetsu, children: nil),
-            SidebarContent(name: "ダイヤ", icon: "folder", detailViewStatus: nil, children: dias)
-        ])]
+    @ViewBuilder
+    func makeDiaListElement(diaNum: Int) -> some View {
+        Text("下り時刻表")
+            .onTapGesture {
+                detailViewStatus = .jikokuhyou(houkou: .kudari, diaNum: diaNum)
+            }
+        Text("上り時刻表")
+            .onTapGesture {
+                detailViewStatus = .jikokuhyou(houkou: .nobori, diaNum: diaNum)
+            }
+        Text("ダイヤグラム")
+            .onTapGesture {
+                detailViewStatus = .diagram
+            }
     }
-}
-
-struct SidebarContent: Identifiable {
-    let id = UUID()
-    let name: String
-    let icon: String
-    let detailViewStatus: DetailViewStatus?
-    let children: [SidebarContent]?
 }
 
 #Preview {
