eab9887582ea8fcaefefd31376ef206f935f78c0
yu3san3 <orange12817@yahoo.co.jp>
2023-12-06 19:37:59 +0900

    SidebarViewを初期状態で展開させた。 ただし、Diaの要素数が10を超えるとアプリが落ちる。

--- DiagramEditor/View/SidebarView.swift
+++ DiagramEditor/View/SidebarView.swift
@@ -12,20 +12,34 @@ struct SidebarView: View {
 
     @Binding var detailViewStatus: DetailViewStatus
 
+    @State private var isRosenExpanded = true
+    @State private var isDiaExpanded = true
+    //FIXME: - ⚠️Diaの要素数が10を超えるとindexがオーバーフローする
+    @State private var isDiaContentExpanded: [Bool] = Array(repeating: true, count: 10)
+
+//    init(detailViewStatus: Binding<DetailViewStatus>) {
+//        self._detailViewStatus = detailViewStatus
+//        self._isDiaContentExpanded = State(initialValue: Array(repeating: true,
+//                                                               count: 10)
+//        )
+//    }
+
     var body: some View {
         List(selection: $detailViewStatus) {
-            DisclosureGroup("路線") {
+            DisclosureGroup("路線", isExpanded: $isRosenExpanded) {
                 NavigationLink(value: DetailViewStatus.eki) {
                     Text("駅")
                 }
                 NavigationLink(value: DetailViewStatus.ressyasyubetsu) {
                     Text("列車種別")
                 }
-                DisclosureGroup("ダイヤ") {
+                DisclosureGroup("ダイヤ", isExpanded: $isDiaExpanded) {
                     ForEach( Array(document.oudData.rosen.dia.enumerated() ),
                             id: \.1.id
                     ) { index, dia in
-                        DisclosureGroup(dia.diaName) {
+                        DisclosureGroup(dia.diaName,
+                                        isExpanded: $isDiaContentExpanded[index]
+                        ) {
                             NavigationLink(
                                 value: DetailViewStatus.kudariJikokuhyou(diaNum: index)
                             ) {
