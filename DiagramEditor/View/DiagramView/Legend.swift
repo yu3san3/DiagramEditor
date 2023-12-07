1435304e5d5d089d6a7ba5b59bb35b92bcc6d69a
yu3san3 <orange12817@yahoo.co.jp>
2023-12-07 16:12:14 +0900

    Legend.swiftを追加

---
+++ DiagramEditor/View/DiagramView/Legend.swift
@@ -0,0 +1,18 @@
+//
+//  Legend.swift
+//  DiagramEditor
+//
+//  Created by 丹羽雄一朗 on 2023/12/07.
+//
+
+import SwiftUI
+
+struct Legend: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Legend()
+}
