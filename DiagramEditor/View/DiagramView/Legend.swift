a33058c783f96a7ca17698e37eee08da022e0963
yu3san3 <orange12817@yahoo.co.jp>
2023-12-08 17:32:13 +0900

    Legendの試作品ができた。 ただ縦線を24本描画するだけ。

--- DiagramEditor/View/DiagramView/Legend.swift
+++ DiagramEditor/View/DiagramView/Legend.swift
@@ -9,10 +9,35 @@ import SwiftUI
 
 struct Legend: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            ForEach(0..<24) { _ in
+                drawVLine(interval: 60)
+            }
+        }
+    }
+
+    @ViewBuilder
+    func drawVLine(interval: CGFloat) -> some View {
+        let lineWidth: CGFloat = 1
+        VLine()
+            .stroke(Color.gray.opacity(0.5), lineWidth: lineWidth)
+            .frame(width: lineWidth, height: 500)
+        Spacer()
+            .frame(width: interval - lineWidth )
+    }
+}
+
+struct VLine: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+        return path
     }
 }
 
 #Preview {
-    Legend()
+    ScrollView([.horizontal, .vertical]) {
+        Legend()
+    }.frame(width: 320, height: 200)
 }
