72ba8cf01fc0302833fc97e9733866ac55f0112c
yu3san3 <orange12817@yahoo.co.jp>
2023-12-16 21:16:19 +0900

    DrawDiagram.swiftを追加

---
+++ DiagramEditor/View/DiagramView/DrawDiagram.swift
@@ -0,0 +1,18 @@
+//
+//  DrawDiagram.swift
+//  DiagramEditor
+//
+//  Created by 丹羽雄一朗 on 2023/12/16.
+//
+
+import SwiftUI
+
+struct DrawDiagram: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrawDiagram()
+}
