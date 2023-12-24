9c0ca09c04b6d89c0b4568e3fc6a249f28b5c961
yu3san3 <orange12817@yahoo.co.jp>
2023-12-25 00:44:35 +0900

    スクロールが激重なのは変わりないが、noboriもLazyHStackを用いたものに換装した。

--- DiagramEditor/View/TimeTableView/JikokuView.swift
+++ DiagramEditor/View/TimeTableView/JikokuView.swift
@@ -19,13 +19,18 @@ struct JikokuView: View {
         switch houkou {
         case .kudari:
             LazyVStack(spacing: 0) {
-                jikoku(ressyas: document.oudData.rosen.dia[diaNum].kudari.ressya,
+                let ressyas = document.oudData.rosen.dia[diaNum].kudari.ressya
+                jikoku(ressyas: ressyas,
                        ekis: document.oudData.rosen.eki)
-                makeBikouCell(text: "hoge")
+                bikou(ressyas: ressyas)
             }
         case .nobori:
-            makeJikokuView(ressyas: document.oudData.rosen.dia[diaNum].nobori.ressya,
-                           ekis: document.oudData.rosen.eki.reversed() )
+            LazyVStack(spacing: 0) {
+                let ressyas = document.oudData.rosen.dia[diaNum].nobori.ressya
+                jikoku(ressyas: ressyas,
+                       ekis: document.oudData.rosen.eki.reversed())
+                bikou(ressyas: ressyas)
+            }
         }
     }
 
@@ -36,9 +41,13 @@ struct JikokuView: View {
                             count: columnCellCount)
         LazyHGrid(rows: columns, spacing: 0) {
             ForEach(ressyas) { ressya in
-                let array = Array( zipLongest(ekis, ressya.ekiJikoku) )
-                ForEach(array, id: \.1?.id) { eki, jikoku in
-                    makeJikokuCell(eki: eki, jikoku: jikoku, ressya: ressya)
+                ForEach( Array(ressya.ekiJikoku.enumerated() ),
+                         id: \.element.id
+                ) { index, jikoku in
+                    let eki = ekis[index]
+                    jikokuCell(ekijikokukeisiki: eki.ekijikokukeisiki,
+                               jikoku: jikoku,
+                               houkou: ressya.houkou)
                 }
                 .font(.caption)
                 .frame(
@@ -50,35 +59,36 @@ struct JikokuView: View {
         }
     }
 
-    func makeJikokuView(ressyas: [Ressya], ekis: [Eki]) -> some View {
+    func bikou(ressyas: [Ressya]) -> some View {
         LazyHStack(spacing: 0) {
             ForEach(ressyas) { ressya in
-                LazyVStack(spacing: 0) {
-                    let array = Array( zipLongest(ekis, ressya.ekiJikoku) )
-                    ForEach(array, id: \.1?.id) { eki, jikoku in
-                        makeJikokuCell(eki: eki, jikoku: jikoku, ressya: ressya)
-                    }
-                    .font(.caption)
-                    .frame(
-                        width: table.jikokuWidth,
-                        height: table.jikokuHeight
-                    )
-                    .border(Color.green)
-                    makeBikouCell(text: ressya.bikou)
+                VStack {
+                    VText(ressya.bikou)
+                        .font(.caption)
+                        .padding(3)
+                    Spacer()
                 }
+                .frame(
+                    width: table.jikokuWidth,
+                    height: table.bikouHeight
+                )
+                .border(Color.yellow)
             }
         }
     }
 
     @ViewBuilder
     //MARK: - 上り、下りの両方できちんと表示されるようにしよう
-    func makeJikokuCell(eki: Eki?, jikoku: Jikoku?, ressya: Ressya) -> some View {
-        switch eki?.ekijikokukeisiki {
+    func jikokuCell(ekijikokukeisiki: Ekijikokukeisiki,
+                    jikoku: Jikoku,
+                    houkou: Houkou
+    ) -> some View {
+        switch ekijikokukeisiki {
         case .hatsuchaku:
-            switch jikoku?.arrivalStatus {
+            switch jikoku.arrivalStatus {
             case .stop:
-                Text(jikoku?.chaku ?? "nil")
-                Text(jikoku?.hatsu ?? "nil")
+                Text(jikoku.chaku)
+                Text(jikoku.hatsu)
             case .pass:
                 Text("ﾚ")
                 Text("ﾚ")
@@ -88,87 +98,42 @@ struct JikokuView: View {
             case .notGoThrough:
                 Text("||")
                 Text("||")
-            case .none:
-                Text("none")
-                Text("none")
             }
         case .hatsu:
-            Text(jikoku?.hatsu ?? "nil")
+            Text(jikoku.hatsu)
         case .kudariChaku:
-            switch ressya.houkou {
+            switch houkou {
             case .kudari:
-                switch jikoku?.arrivalStatus {
+                switch jikoku.arrivalStatus {
                 case .stop:
-                    Text(jikoku?.chaku ?? "nil")
+                    Text(jikoku.chaku)
                 case .pass:
                     Text("ﾚ")
                 case .notOperate:
                     Text("･･")
                 case .notGoThrough:
                     Text("||")
-                case .none:
-                    Text("none")
                 }
             case .nobori:
-                Text(jikoku?.hatsu ?? "nil")
+                Text(jikoku.hatsu)
             }
         case .noboriChaku:
-            switch ressya.houkou {
+            switch houkou {
             case .kudari:
-                switch jikoku?.arrivalStatus {
+                switch jikoku.arrivalStatus {
                 case .stop:
-                    Text(jikoku?.hatsu ?? "nil")
+                    Text(jikoku.hatsu)
                 case .pass:
                     Text("ﾚ")
                 case .notOperate:
                     Text("･･")
                 case .notGoThrough:
                     Text("||")
-                case .none:
-                    Text("none")
                 }
             case .nobori:
-                Text(jikoku?.chaku ?? "nil")
+                Text(jikoku.chaku)
             }
-        case .none:
-            Text("none")
         }
-    }
-
-    func makeBikouCell(text: String) -> some View {
-        VStack { //備考
-            VText(text)
-                .font(.caption)
-                .padding(3)
-            Spacer()
-        }
-        .frame(
-            width: table.jikokuWidth,
-            height: table.bikouHeight
-        )
-        .border(Color.yellow)
-    }
-}
-
-private func zipLongest<T, U>(_ first: [T], _ second: [U]) -> AnyIterator<(T?, U?)> {
-    // インデックスとイテレータを初期化する
-    var index = 0
-    var firstIterator = first.makeIterator()
-    var secondIterator = second.makeIterator()
-
-    // イテレータを返すクロージャを定義する
-    return AnyIterator {
-        // 両方のシーケンスが終了したらnilを返す
-        if index >= first.count && index >= second.count {
-            return nil
-        }
-        // インデックスに対応する要素を取得する
-        let firstElement = index < first.count ? firstIterator.next() : nil
-        let secondElement = index < second.count ? secondIterator.next() : nil
-        // インデックスをインクリメントする
-        index += 1
-        // 要素のペアを返す
-        return (firstElement, secondElement)
     }
 }
 
