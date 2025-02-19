5aac5ffcb56d8a0f014c05eb554e4fdd46df34b5
yu3san3 <orange12817@yahoo.co.jp>
2025-02-20 01:24:48 +0900

    [delete, update] 時刻表に表示する時刻データを配列で得られるようメソッドを実装

--- DiagramEditor/DiagramEditorDocument.swift
+++ DiagramEditor/DiagramEditorDocument.swift
@@ -9,6 +9,82 @@ import OuDiaKit
 import UniformTypeIdentifiers
 import SwiftUI
 
+struct ZipLongestSequence<Sequence1: Sequence, Sequence2: Sequence>: Sequence, IteratorProtocol {
+    private var sequence1Iterator: Sequence1.Iterator
+    private var sequence2Iterator: Sequence2.Iterator
+
+    init(_ sequence1: Sequence1, _ sequence2: Sequence2) {
+        self.sequence1Iterator = sequence1.makeIterator()
+        self.sequence2Iterator = sequence2.makeIterator()
+    }
+
+    mutating func next() -> (Sequence1.Element?, Sequence2.Element?)? {
+        let sequence1Value = sequence1Iterator.next()
+        let sequence2Value = sequence2Iterator.next()
+        return (sequence1Value == nil && sequence2Value == nil) ? nil : (sequence1Value, sequence2Value)
+    }
+}
+
+extension Sequence {
+    func zipLongest<S: Sequence>(_ other: S) -> ZipLongestSequence<Self, S> {
+        ZipLongestSequence(self, other)
+    }
+}
+
+extension Array {
+    func reversed(shouldReverse: Bool) -> [Element] {
+        shouldReverse ? self.reversed() : self
+    }
+}
+
+extension ArrivalStatus {
+    var timetableText: String {
+        switch self {
+        case .notOperate: "･･"
+        case .stop: "⚪︎"
+        case .pass: "ﾚ"
+        case .notGoThrough: "||"
+        }
+    }
+}
+
+extension Train {
+    /// 時刻表に表示すべき時刻データを得る。
+    ///
+    /// - Parameter timeTypes: 駅の配列から駅時刻形式 (`timeType`) のみを抜き出した配列。列車の方向 (上り・下り) を考慮して逆順にする必要はない。
+    /// - Returns: 時刻表に表示すべき時刻データの配列。
+    func timeTableText(timeTypes: [StationTimeType]) -> [String] {
+        timeTypes
+            .reversed(shouldReverse: direction == .up)
+            .zipLongest(schedule)
+            .flatMap { timeType, scheduleEntry in
+                guard let timeType else { fatalError("timeTypeは仕様上`nil`になってはいけない。") }
+
+                let arrivalText = scheduleEntry?.arrival
+                ?? scheduleEntry?.arrivalStatus.timetableText
+                ?? ArrivalStatus.notOperate.timetableText
+
+                let departureText = scheduleEntry?.departure
+                ?? scheduleEntry?.arrivalStatus.timetableText
+                ?? ArrivalStatus.notOperate.timetableText
+
+                switch timeType {
+                case .departure:
+                    return [departureText]
+                case .arrivalDeparture:
+                    return [arrivalText, departureText]
+                case .downArrival, .upArrival:
+                    let isDownArrival = timeType == .downArrival
+                    let isTrainDownDirection = direction == .down
+
+                    // 駅が下り着で下り列車、もしくは駅が上り着で上り列車の場合には、着時刻を使用
+                    let shouldUseArrival = (isDownArrival && isTrainDownDirection) || (!isDownArrival && !isTrainDownDirection)
+                    return [shouldUseArrival ? arrivalText : departureText]
+                }
+            }
+    }
+}
+
 extension UTType {
     static var oudiaDocument: UTType {
         UTType(importedAs: "com.takeokm.oudia.text")
